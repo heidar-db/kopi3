1028144cd0e7a0c11dd8fc5efeac5217b8af88d1
heidarfly <heidar@handalin.com>
2025-02-03 16:34:41 +0700

    drubah dikit

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 			include 'admin/template/header.php';
 			include 'admin/template/sidebar.php';
 				if(!empty($_GET['page'])){
-					include 'admin/module/'.$_GET['page'].'/index.php';
+					include 'admin/module/'.$_GET['page'].'/heidar.php';
 				}else{
 					include 'admin/template/home.php';
 				}
